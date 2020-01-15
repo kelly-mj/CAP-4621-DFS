db98e7a95978f98c8ae4c8c78ee4fb407613f2aa
Kelly MJ <kellymj033@gmail.com>
2020-01-15 16:03:38 -0500

    added DFS

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ util.setup()
 
 # TODO: prompt user for input
 # test input
-start = "B2"
-end   = "E4"
+start = "A1"
+end   = "G1"
 
 # do DFS to find path from start to end
+path = [start]
+visited = [start]
 
+while(path[-1] != end):
+  change = 0  # flag to indicate whether any unvisited neighbors were found
+  # iterate over neighbors of current node
+  for x in util.connections[path[-1]]:
+    # if neighbor has not been visited yet, add neighbor to the path
+    if x not in visited:
+      curr = x
+      visited.append(x)
+      path.append(x)
+      change = 1
+      break
+  # if no unvisited neighbors were found, pop current node from the stack
+  if change == 0:
+    path.pop()
+
+print(path)
