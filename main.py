eb8fafbf42c696426f21adbc956ae80c3cde33a0
philipchang411 <philipchang@mail.usf.edu>
2020-01-15 11:57:12 -0500

    Read Location File
    Create a dictionary from reading in the location file.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import math
+
 # declare dictionaries
 connections = {}
 locations = {}
 
+# read contents from "locations.txt" into a dictionary
+location = open("locations.txt").read().split("\n")
+for line in location:
+  location = line.split(" ")
+  locations[location[0]] = location[1:]
+  
 # read contents of "connections.txt" into dictionary
 c = open("connections.txt").read().split("\n")
 for line in c:
   l = line.split(" ")
   connections[l[0]] = l[2:]
-
+      
+print(locations)
+print("\n")
 print(connections)
