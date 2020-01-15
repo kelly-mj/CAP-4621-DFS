3dd97649d689e7b81548a6d591d49a1fdf7588e0
Kelly MJ <kellymj033@gmail.com>
2020-01-15 11:40:58 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# declare dictionaries
+connections = {}
+locations = {}
+
+# read contents of "connections.txt" into dictionary
+c = open("connections.txt").read().split("\n")
+for line in c:
+  l = line.split(" ")
+  connections[l[0]] = l[2:]
+
+print(connections)
