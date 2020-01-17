a6c4a3505d5b862de48a7dd4280dabaebdc695f2
philipchang411 <philipchang@mail.usf.edu>
2020-01-16 23:21:17 -0500

    Calculate Distance
    Uses the path to calculate the total distance travelled using the distance formula

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import util as util
-
+import math
 # import/parse location and connection file contents
 util.setup()
 
@@ -11,6 +11,7 @@ end   = "G1"
 # do DFS to find path from start to end
 path = [start]
 visited = [start]
+distances = []
 
 while(path[-1] != end):
   change = 0  # flag to indicate whether any unvisited neighbors were found
@@ -27,4 +28,25 @@ while(path[-1] != end):
   if change == 0:
     path.pop()
 
-print(path)
+for i in range(0, len(path)-1):
+      points = []
+      for value in util.locations[path[i]]:
+            points.append(value)
+      for value in util.locations[path[i+1]]:
+            points.append(value)
+      x1 = int(points[0])
+      x2 = int(points[2])
+      y1 = int(points[1])
+      y2 = int(points[3])
+      distance = math.sqrt((x2-x1)**2+(y2-y1)**2)
+      distances.append(distance)
+      #print(points)
+#print(distances)
+sum = 0
+for i in distances:
+      sum = sum + i
+      
+print(sum)
+            
+            
+#print(path)
